5f8f82c5aae9525bbdaac78e13abbb35571b9a04
Dominik Meise <im.od@hotmail.com>
2019-05-15 15:26:06 +0200

    Implemented canny example

---
+++ oct_preprocessing.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from skimage import io, color
+from skimage.feature import canny
+matplotlib.rcParams['image.cmap'] = 'gray'
+matplotlib.rcParams['image.interpolation'] = 'nearest'
+
+
+def main():
+    simple_canny_example('Train-Data/SRF/input_1492_1.png')
+    simple_binarization_example('Train-Data/SRF/input_1492_1.png')
+
+
+def simple_binarization_example(filepath):
+    pass
+
+
+def simple_canny_example(filepath):
+    orig = io.imread(filepath)
+
+    sigma = 3
+    orig_canny = canny(color.rgb2gray(orig), sigma)
+
+    plot_original_and_processed(orig, orig_canny, 'canny edge with sigma = {}'.format(sigma))
+
+
+def plot_original_and_processed(original, processed, process_title):
+
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].imshow(original)
+    axs[0].set_title('original image')
+    axs[1].imshow(processed)
+    axs[1].set_title(process_title)
+
+    fig.suptitle('example image for SRF')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
