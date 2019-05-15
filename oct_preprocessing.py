13d72b2d0d1d8eb7cf625c7e5d5208fa2b2adedd
Dominik Meise <im.od@hotmail.com>
2019-05-15 16:22:16 +0200

    Started implementing initial binarization for later cropping

--- oct_preprocessing.py
+++ oct_preprocessing.py
@@ -1,14 +1,42 @@
+import glob
 import matplotlib
 import matplotlib.pyplot as plt
 from skimage import io, color
 from skimage.feature import canny
-from skimage.filters import try_all_threshold
+from skimage.filters import try_all_threshold, threshold_otsu, gaussian
 matplotlib.rcParams['image.cmap'] = 'gray'
 
 
 def main():
-    simple_canny_example('Train-Data/SRF/input_1492_1.png')
-    simple_binarization_example('Train-Data/SRF/input_1492_1.png')
+    ###     VARIOUS TESTING
+    #simple_canny_example('Train-Data/SRF/input_1492_1.png')
+    #simple_binarization_example('Train-Data/SRF/input_1492_1.png')
+    #images = glob.glob('Train-Data/SRF/*')
+    #images = images + glob.glob('Train-Data/NoSRF/*')
+    #for i in images[:3]:
+    #    orig = io.imread(i)
+    #    orig_bin = otsu_binar(orig)
+    #    plot_original_and_processed(orig, orig_bin, 'otsu')
+
+    # template for running task:
+    for images in list:
+        img = io.imread(path)
+        cropped_img = crop(img)
+        # canny? whatever?...
+
+
+def crop():
+    pass
+
+
+def otsu_binar(orig):
+
+    orig = color.rgb2gray(orig)
+    orig_blur = gaussian(orig, sigma=2)
+    otsu_thresh = threshold_otsu(orig_blur)
+    orig_bin = orig_blur > otsu_thresh
+
+    return orig_bin
 
 
 def simple_binarization_example(filepath):
@@ -23,7 +51,7 @@ def simple_binarization_example(filepath):
 def simple_canny_example(filepath):
     orig = io.imread(filepath)
 
-    sigma = 3
+    sigma = 7
     orig_canny = canny(color.rgb2gray(orig), sigma)
 
     plot_original_and_processed(orig, orig_canny, 'canny edge with sigma = {}'.format(sigma))
