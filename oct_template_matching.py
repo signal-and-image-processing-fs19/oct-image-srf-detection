c539c2c7d7dda64a7240b245b7d6fbace4e8890e
Dominik Meise <im.od@hotmail.com>
2019-05-22 18:23:20 +0200

    Implemented template_matching() and opening_denoising()

--- oct_template_matching.py
+++ oct_template_matching.py
@@ -17,31 +17,44 @@ __email__ = "dominik.meise@students.unibe.ch"
 
 
 import cv2 as cv
-from skimage import io
 
-def template_matching(img, path):
-    img2 = img.copy()
-    template = io.imread('dummy_template.png')
+
+def template_matching(image, template, meth='cv.TM_SQDIFF'):
+    """Match the template against the image, return resolution and location map.
+
+    :param image: input image in which to search for the template matching (either rgb or grayscale)
+    :param template: kernel/template which to match against the input image
+    :param meth: template matching method. one of the following:
+        'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+        'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+    :return: res: the distance map of the template matching the input image
+                  (depending on the method either higher or lower is better)
+             img: image with a square at the location of the best match
+    """
+    img = image.copy()
     w, h = template.shape[::-1]
+    method = eval(meth)
 
-    # All the 6 methods for comparison in a list
-    methods = ['cv.TM_SQDIFF']  # 'cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR', 'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED'
+    # Apply template Matching
+    res = cv.matchTemplate(img, template, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-    for meth in methods:
-        img = img2.copy()
-        method = eval(meth)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
 
-        # Apply template Matching
-        res = cv.matchTemplate(img, template, method)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    cv.rectangle(img, top_left, bottom_right, 255, 2)
 
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-        else:
-            top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
+    return res, img
 
-        cv.rectangle(img, top_left, bottom_right, 255, 2)
 
-        return res, img, meth
+def opening_denoising(img, kernel_size=5):
+    """Denoise image by opening (erosion then dilation)."""
+
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernel_size, kernel_size))
+    img_denoise = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+
+    return img_denoise
